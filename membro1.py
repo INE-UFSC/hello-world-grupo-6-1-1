38a4d283a8837022ff77732bc8d036ad147fa636
Victor Gouvêa <victorgouvea28@gmail.com>
2021-10-26 11:13:57 -0300

    Atualizando minha informações no membro1.py

--- membro1.py
+++ membro1.py
@@ -1,2 +1,4 @@
 def hello():
-	return "Olá, eu sou o membro1" #escreva aqui a sua apresentação - nome, idade, hobbies, experiência prévia de programação, e o que você espera desta disciplina
+	return "Olá, eu sou o Victor, tenho 19 anos. Meus hobbies são principalmente jogar online e acompanhar Formula 1. Minha experiência prévia em programação se resume apenas no que aprendi no semestre passado, e espero aprimorar esses conceitos nessa discplina." #escreva aqui a sua apresentação - nome, idade, hobbies, experiência prévia de programação, e o que você espera desta disciplina
+
+print(hello())
