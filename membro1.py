0517a9c79bc492cb25854c1c9880fd014ccdbc3c
sammycosta <86506839+sammycosta@users.noreply.github.com>
2021-10-26 11:17:14 -0300

    Resolução de print repetido

--- membro1.py
+++ membro1.py
@@ -1,4 +1,3 @@
 def hello():
-	return "Olá, eu sou o Victor, tenho 19 anos. Meus hobbies são principalmente jogar online e acompanhar Formula 1. Minha experiência prévia em programação se resume apenas no que aprendi no semestre passado, e espero aprimorar esses conceitos nessa discplina." #escreva aqui a sua apresentação - nome, idade, hobbies, experiência prévia de programação, e o que você espera desta disciplina
-
-print(hello())
+    # escreva aqui a sua apresentação - nome, idade, hobbies, experiência prévia de programação, e o que você espera desta disciplina
+    return "Olá, eu sou o Victor, tenho 19 anos. Meus hobbies são principalmente jogar online e acompanhar Formula 1. Minha experiência prévia em programação se resume apenas no que aprendi no semestre passado, e espero aprimorar esses conceitos nessa discplina."
